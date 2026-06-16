a8abd71231d6cf68cc8b70f0513e4d2037b81e11
mostafa22khaled <mostafa2272005khaled@gmail.com>
2026-06-16 19:36:33 +0300

    Add category filtering

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,12 @@
 import { useEffect, useState } from "react";
 import ProductCard from "../components/ProductCard";
+import SearchBar from "../components/SearchBar";
+
 function Home() {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [search, setSearch] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("all");
 
   useEffect(() => {
     fetch("https://fakestoreapi.com/products")
@@ -13,17 +17,55 @@ function Home() {
       });
   }, []);
 
+  const categories = [
+    "all",
+    ...new Set(products.map((product) => product.category)),
+  ];
+
+  const filteredProducts = products.filter((product) => {
+    const matchesSearch = product.title
+      .toLowerCase()
+      .includes(search.toLowerCase());
+
+    const matchesCategory =
+      selectedCategory === "all" || product.category === selectedCategory;
+
+    return matchesSearch && matchesCategory;
+  });
+
   if (loading) {
     return <h1>Loading...</h1>;
   }
 
   return (
     <div>
-      <h1>Products ({products.length})</h1>
+      <h1>Products ({filteredProducts.length})</h1>
 
-      {products.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
+      <SearchBar search={search} setSearch={setSearch} />
+
+      <br />
+
+      <select
+        value={selectedCategory}
+        onChange={(e) => setSelectedCategory(e.target.value)}
+      >
+        {categories.map((category) => (
+          <option key={category} value={category}>
+            {category}
+          </option>
+        ))}
+      </select>
+
+      <br />
+      <br />
+
+      {filteredProducts.length === 0 ? (
+        <h2>No Results Found</h2>
+      ) : (
+        filteredProducts.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))
+      )}
     </div>
   );
 }
